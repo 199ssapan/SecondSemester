2293d56f6f11e2ca18bcb2fe1b2db516504fd68a
PetrovMaksim2004 <maksman1987@mail.ru>
2023-03-19 22:38:41 +0500

    19.03

---
+++ GameOfLife/build/GOFLib/CMakeFiles/GOFLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GOFLib.dir/GOF.c.o"
+  "libGOFLib.a"
+  "libGOFLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GOFLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
